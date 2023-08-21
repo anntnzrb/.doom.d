38918374a96ced0ca85b7e840645baa02ed36127
anntnzrb <anntnzrb@protonmail.com>
2023-08-21 18:21:02 -0500

    chore(bootstrap): minor adds

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,5 +10,8 @@ DOOM_BRANCH="master"
 
 git clone --depth 1 --no-tags --branch "${DOOM_BRANCH}" "${DOOM_REPO}" "${EMACS_DIR}"
 
-"${EMACS_DIR}/bin/doom" install --force
+"${EMACS_DIR}/bin/doom" install --force --benchmark --color
+"${EMACS_DIR}/bin/doom" upgrade --force --benchmark --color
+"${EMACS_DIR}/bin/doom" sync --force --benchmark --color -u -p
+"${EMACS_DIR}/bin/doom" doctor --force --benchmark --color
 
