98027c20dcb5649a609a086cc7654619a27d4f52
anntnzrb <anntnzrb@protonmail.com>
2024-01-19 15:40:38 -0500

    refactor(bootstrap.sh)

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,21 @@
 # bootstrap.sh --- Bootstrap Doom Emacs
 
 EMACS_DIR="${HOME}/.config/emacs"
-DOOM_REPO="https://github.com/doomemacs/doomemacs"
-DOOM_BRANCH="master"
+DOOM_REPO='https://github.com/doomemacs/doomemacs'
+DOOM_BRANCH='master'
+DOOM_BIN="${EMACS_DIR}/bin/doom"
+DOOM_FLAGS='--verbose --force --benchmark --color'
+
+die() {
+    printf "${@}\n" >&2
+    exit 1
+}
 
 # -----------------------------------------------------------------------------
 
-git clone --depth 1 --no-tags --branch "${DOOM_BRANCH}" "${DOOM_REPO}" "${EMACS_DIR}"
+git clone --depth 1 --no-tags --branch "${DOOM_BRANCH}" "${DOOM_REPO}" "${EMACS_DIR}" || :
 
-"${EMACS_DIR}/bin/doom" install --force --benchmark --color
-"${EMACS_DIR}/bin/doom" upgrade --force --benchmark --color
-"${EMACS_DIR}/bin/doom" sync --force --benchmark --color -u -p
-"${EMACS_DIR}/bin/doom" doctor --force --benchmark --color
-
+"${DOOM_BIN}" install ${DOOM_FLAGS} || die 'Failed to install Doom Emacs'
+"${DOOM_BIN}" upgrade ${DOOM_FLAGS} || die 'Failed to upgrade Doom Emacs'
+"${DOOM_BIN}" sync ${DOOM_FLAGS} -u -p ||  die 'Failed to sync Doom Emacs'
+"${DOOM_BIN}" doctor
