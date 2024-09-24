37729824169314d825f2d8354e6ced4365ca29fc
anntnzrb <anntnzrb@proton.me>
2024-09-24 04:12:04 -0500

    bootstrap: update

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -9,15 +9,15 @@ DOOM_BIN="${EMACS_DIR}/bin/doom"
 DOOM_FLAGS='--verbose --force --benchmark --color'
 
 die() {
-    printf "${@}\n" >&2
-    exit 1
+	printf "%s\n" "${@}" >&2
+	exit 1
 }
 
 # -----------------------------------------------------------------------------
 
 git clone --depth 1 --no-tags --branch "${DOOM_BRANCH}" "${DOOM_REPO}" "${EMACS_DIR}" || :
 
-"${DOOM_BIN}" install ${DOOM_FLAGS} || die 'Failed to install Doom Emacs'
-"${DOOM_BIN}" upgrade ${DOOM_FLAGS} || die 'Failed to upgrade Doom Emacs'
-"${DOOM_BIN}" sync ${DOOM_FLAGS} -u -p ||  die 'Failed to sync Doom Emacs'
+"${DOOM_BIN}" install ${DOOM_FLAGS} --aot || die 'Failed to install Doom Emacs'
+"${DOOM_BIN}" upgrade ${DOOM_FLAGS} --aot || die 'Failed to upgrade Doom Emacs'
+"${DOOM_BIN}" sync ${DOOM_FLAGS} --aot --gc -u || die 'Failed to sync Doom Emacs'
 "${DOOM_BIN}" doctor
